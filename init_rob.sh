d65e882ffe2d7f5f2b8542580213ed76a58841da
robertwan <robertwan@tencent.com>
2020-12-23 19:37:34 +0800

    add idc

--- init_rob.sh
+++ init_rob.sh
@@ -1,3 +1,29 @@
 #/bin/bash
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+cd "$SHELL_FOLDER"
 
-cp ./vimrc_idc ~/.
+if [ "$1" = "idc" ]; then
+    echo "init_idc"
+    echo "cp ./vimrc_idc ~/"
+    mv ~/.vimrc ~/.vimrc_back
+    cp ./vimrc_idc ~/.vimrc
+
+    mkdiri -p ~/.vim/autoload/
+    cp ./plug.vim ~/.vim/autoload/
+
+    echo "clone plugin"
+    git clone https://git::@github.com/mhinz/vim-grepper
+    git clone https://git::@github.com/jonathanfilip/vim-lucius
+    git clone https://git::@github.com/elzr/vim-json.git
+    mkdir -p ~/.vim/plugged
+    mv vim-grepper  ~/.vim/plugged
+    mv vim-json  ~/.vim/plugged
+    mv vim-luciusr  ~/.vim/plugged
+
+    echo "install fzf"
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+     ~/.fzf/install
+
+    echo "install plugin"
+    vim +PlugInstall +qall; echo $?
+fi
